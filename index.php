bd6031d191f5f7ec47dcb6b267849ce1a75e86cb
AngeTia <tiaange03@gmail.com>
2023-01-26 13:09:59 +0000

    Update file

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <form action="confirmForm.php" method="post">
+            <p>First name: <input type="text" name="firstname" /></p>
+            <label for="color"> Color :</label>
+            <select name="color">
+                <option value="Blue">Blue</option>
+                <option value="Red">Red</option>
+                <option value="Green">Green</option>
+            </select>
+            <input type="submit" name="submit" value="Submit" />
+        </form>    
+    </body>
+</html>
